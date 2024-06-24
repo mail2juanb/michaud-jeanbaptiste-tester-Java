ad7f7c4664c93a5f9276a064b562be6631ed1464
juanb <mail2juanb@yahoo.fr>
2024-06-24 14:52:31 +0200

    test(ParkingSpotTest): Unit test for getId. The Id is correctly defined and retrieved.

---
+++ src/test/java/com/parkit/parkingsystem/ParkingSpotTest.java
@@ -0,0 +1,25 @@
+package com.parkit.parkingsystem;
+
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class ParkingSpotTest {
+
+    @Test
+    @DisplayName("The Id value is correctly defined and retrieved.")
+    public void getIdParkingSpotTest() {
+        //GIVEN an Id of ParkingSpot
+        int expectedIdParkingSpot = 1;
+
+        //WHEN define ParkingSpot
+        ParkingSpot parkingSpot = new ParkingSpot(expectedIdParkingSpot, ParkingType.CAR, true);
+
+        //THEN retrieve the correct Id
+        assertEquals(expectedIdParkingSpot, parkingSpot.getId());
+    }
+
+}
